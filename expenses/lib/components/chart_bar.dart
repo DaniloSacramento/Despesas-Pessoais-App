c17581b82f8e90566856e50098656c2609ae234d
Danilo Sacramento <89158678+DaniloSacramento@users.noreply.github.com>
2022-10-03 17:12:10 -0300

    chart widget 3

--- expenses/lib/components/chart_bar.dart
+++ expenses/lib/components/chart_bar.dart
@@ -21,7 +21,27 @@ class ChartBar extends StatelessWidget {
         Container(
           height: 60,
           width: 10,
-          child: null,
+          child: Stack(
+            children: [
+              Container(decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.grey,
+                  width: 1.0,
+                ) ,
+                color: Color.fromRGBO(220, 220, 220, 1),
+                borderRadius: BorderRadius.circular(5)
+              ),
+              ),
+              FractionallySizedBox(
+                heightFactor: percentage,
+                child: Container(decoration: BoxDecoration(
+                 color: Theme.of(context).colorScheme.primary,
+                 borderRadius: BorderRadius.circular(5)
+                )
+                ),
+                )
+            ],
+          ),
         ),
         SizedBox(height: 5),
         Text(label!),
